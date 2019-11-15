71516fedb32aec1eaf4a75ceef56c31a40df23e5
konradgryt <konrad.gryt@gmail.com>
2019-11-15 20:34:04 +0100

    Initial map setup

--- scrappy-map.js
+++ scrappy-map.js
@@ -1,6 +1,51 @@
-mapboxgl.accessToken = 'pk.eyJ1Ijoic2NyYXBweXBsZWFzZSIsImEiOiJjazMwZ3c4OGEwN2pyM25yY3U0N3Fqb2h6In0.ZpsqtTQg6Mbt6iTwqYye2w';
+const ACCESS_TOKEN = "pk.eyJ1Ijoic2NyYXBweXBsZWFzZSIsImEiOiJjazMwZ3c4OGEwN2pyM25yY3U0N3Fqb2h6In0.ZpsqtTQg6Mbt6iTwqYye2w";
 
-var map = new mapboxgl.Map({
-container: 'map',
-style: 'mapbox://styles/mapbox/streets-v9'
+mapboxgl.accessToken = ACCESS_TOKEN;
+
+const map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/scrappyplease/ck30ioijq0zat1co8mgfrtfg4",
+  center: [10.212405079081918, 56.15020856422052],
+  zoom: 11
 });
+
+
+const geojson = {
+  type: 'Items to collect',
+  items: [{
+    type: 'Big item',
+    geometry: {
+      type: 'Point',
+      coordinates: [10.183491, 56.127352]
+    },
+    properties: {
+      title: 'Wardrobe',
+      // description: 'Just some wardrobe left on the streets'
+    }
+  },
+  {
+    type: 'Small item',
+    geometry: {
+      type: 'Point',
+      coordinates: [10.212405079081918, 56.15020856422052]
+    },
+    properties: {
+      title: 'Small item',
+      // description: 'Your moms underpants haha'
+    }
+  }]
+}
+
+// THIS CODE IS ADDING PINS TO THE MAP
+geojson.items.forEach(function(marker) {
+
+  // creating element for the map marker/pin/whatever its called
+  var el = document.createElement('div');
+  el.className = 'map-marker';
+
+  // make a marker for each item and add to the map
+new mapboxgl.Marker(el)
+  .setLngLat(marker.geometry.coordinates)
+  .addTo(map);
+});
+  
