44b9b293371a943c9e19f53707864514c3e7b446
konradgryt <konrad.gryt@gmail.com>
2019-11-15 19:45:44 +0100

    Add mapbox gl

--- scrappy-map.js
+++ scrappy-map.js
@@ -1,10 +1,6 @@
-
-
-const mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
-
 mapboxgl.accessToken = 'pk.eyJ1Ijoic2NyYXBweXBsZWFzZSIsImEiOiJjazMwZ3c4OGEwN2pyM25yY3U0N3Fqb2h6In0.ZpsqtTQg6Mbt6iTwqYye2w';
 
-const map = new mapboxgl.Map({
-  container: 'app-map',
-  style: 'mapbox://styles/mapbox/streets-v11'
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/streets-v9'
 });
