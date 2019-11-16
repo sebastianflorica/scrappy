2f5885b3a6b022518fb46ac6cbfb477a44ae380b
Sebastian Florica <sebastian.florica@tradeshift.com>
2019-11-16 09:19:13 +0100

    Visual Recognition online

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
-var express = require('express');
-var cors = require('cors');
-var app = express();
 
-app.use(cors());
+const fs = require('fs');
+const VisualRecognitionV3 = require('ibm-watson/visual-recognition/v3');
+const { IamAuthenticator } = require('ibm-watson/auth');
 
-app.get('/products/:id', function (req, res, next) {
-    res.json({msg: 'This is CORS-enabled for all origins!'})
+const visualRecognition = new VisualRecognitionV3({
+    url: 'https://gateway.watsonplatform.net/visual-recognition/api',
+    version: '2018-03-19',
+    authenticator: new IamAuthenticator({ apikey: 'N1dS58xbeIT2KyN_ndkIxyfoAYJ7TYEr_BXSPYyAtSnH' }),
 });
 
-app.listen(80, function () {
-    console.log('CORS-enabled web server listening on port 80')
-});
+const params = {
+    imagesFile: fs.createReadStream('./images/test.jpg')
+};
+
+visualRecognition.classify(params)
+    .then(response => {
+        console.log(JSON.stringify(response.result, null, 2));
+    })
+    .catch(err => {
+        console.log(err);
+    });
