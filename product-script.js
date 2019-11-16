024655c5736795fbc1898cf3312de59b4bbf5d03
konradgryt <konrad.gryt@gmail.com>
2019-11-16 02:53:54 +0100

    New svgs, divided scripts into seperate files, added modals

---
+++ product-script.js
@@ -0,0 +1,23 @@
+async function getData() {
+  let response = await fetch('http://134.209.242.120');
+  if (response.ok) { 
+    let json = await response.json();
+
+    json.forEach(function(marker) {
+
+      var node = document.createElement("div");  
+      node.classList.add('product');
+      // var img = document.createElement("img");  
+      // img.setAttribute("src", `http://134.209.242.120${marker.imageURL}`);
+      // node.appendChild(img); 
+      node.style.background = `url('http://134.209.242.120${marker.imageURL}') center center`;                           
+      document.getElementById("products").appendChild(node);
+
+    });
+  } else {
+    alert("HTTP-Error: " + response.status);
+  }
+}
+
+getData();
+
--- scrappy-map.js
+++ scrappy-map.js
@@ -1,21 +1,27 @@
-const ACCESS_TOKEN = "pk.eyJ1Ijoic2NyYXBweXBsZWFzZSIsImEiOiJjazMwZ3c4OGEwN2pyM25yY3U0N3Fqb2h6In0.ZpsqtTQg6Mbt6iTwqYye2w";
+var ACCESS_TOKEN = "pk.eyJ1Ijoic2NyYXBweXBsZWFzZSIsImEiOiJjazMwZ3c4OGEwN2pyM25yY3U0N3Fqb2h6In0.ZpsqtTQg6Mbt6iTwqYye2w";
 mapboxgl.accessToken = ACCESS_TOKEN;
 
-const map = new mapboxgl.Map({
+var map = new mapboxgl.Map({
   container: "map",
   style: "mapbox://styles/scrappyplease/ck30ioijq0zat1co8mgfrtfg4",
   center: [10.212405079081918, 56.15020856422052],
-  zoom: 11
+  zoom: 16
 });
+
+
 
 async function getData() {
   let response = await fetch('http://134.209.242.120');
   if (response.ok) { 
     let json = await response.json();
+
     json.forEach(function(marker) {
-    
+
+      var popupHtml = '<img height="200px" width="200px" src="http://134.209.242.120'+ marker.imageURL +'"></img>'
+      var popup = new mapboxgl.Popup({offset: 25}).setHTML(popupHtml);
+
       let el = document.createElement('div');
-      
+
       if (marker.type == 'large' ) {
         el.className = 'map-marker__large';
       } else if (marker.type == 'small' ) {
@@ -26,7 +32,8 @@ async function getData() {
   
       new mapboxgl.Marker(el)
       .setLngLat(marker.geo)
-      .addTo(map);
+      .addTo(map)
+      .setPopup(popup);
     });
   } else {
     alert("HTTP-Error: " + response.status);
