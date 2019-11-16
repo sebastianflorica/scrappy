8a0c2bdb54ad23b2c0cfd81447e9ee2bc7391f08
konradgryt <konrad.gryt@gmail.com>
2019-11-16 03:16:28 +0100

    Update camera.js

--- camera.js
+++ camera.js
@@ -1,3 +1,16 @@
 if('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices){
-  const stream = await navigator.mediaDevices.getUserMedia({video: true})
+  const stream = await navigator.mediaDevices.getUserMedia({
+    video: {
+      width: { 
+        min: 1280,
+        ideal: 1920,
+        max: 2560,
+      },
+      height: {
+        min: 720,
+        ideal: 1080,
+        max: 1440
+      }
+    }
+  })
 }
