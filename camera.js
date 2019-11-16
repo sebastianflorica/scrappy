ed43fb3ed6f6a42fbda8fbf1fa5d30408242cc11
konradgryt <konrad.gryt@gmail.com>
2019-11-16 03:18:05 +0100

    Wrap camera funcion in async

--- camera.js
+++ camera.js
@@ -1,16 +1,20 @@
-if('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices){
-  const stream = await navigator.mediaDevices.getUserMedia({
-    video: {
-      width: { 
-        min: 1280,
-        ideal: 1920,
-        max: 2560,
-      },
-      height: {
-        min: 720,
-        ideal: 1080,
-        max: 1440
+async function askPermission() {
+  if('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices){
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: {
+        width: { 
+          min: 1280,
+          ideal: 1920,
+          max: 2560,
+        },
+        height: {
+          min: 720,
+          ideal: 1080,
+          max: 1440
+        }
       }
-    }
-  })
+    })
+  }
 }
+
+askPermission();
