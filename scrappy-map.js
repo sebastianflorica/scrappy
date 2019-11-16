87c6b04496ee15aec5c1dc18d6996902ac36b9ee
konradgryt <konrad.gryt@gmail.com>
2019-11-16 01:41:39 +0100

    Pin categorizing

--- scrappy-map.js
+++ scrappy-map.js
@@ -1,7 +1,4 @@
-
-
 const ACCESS_TOKEN = "pk.eyJ1Ijoic2NyYXBweXBsZWFzZSIsImEiOiJjazMwZ3c4OGEwN2pyM25yY3U0N3Fqb2h6In0.ZpsqtTQg6Mbt6iTwqYye2w";
-
 mapboxgl.accessToken = ACCESS_TOKEN;
 
 const map = new mapboxgl.Map({
@@ -11,61 +8,22 @@ const map = new mapboxgl.Map({
   zoom: 11
 });
 
-const geojson = {
-  type: 'Items to collect',
-  items: [{
-    type: 'Large',
-    geometry: {
-      type: 'Point',
-      coordinates: [10.183491, 56.127352]
-    },
-    properties: {
-      title: 'Wardrobe',
-      // description: 'Just some wardrobe left on the streets'
-    }
-  },
-  {
-    type: 'Medium',
-    geometry: {
-      type: 'Point',
-      coordinates: [10.193591, 56.127352]
-    },
-    properties: {
-      title: 'Wardrobe',
-      // description: 'Just some wardrobe left on the streets'
-    }
-  },
-  {
-    type: 'Small',
-    geometry: {
-      type: 'Point',
-      coordinates: [10.212405079081918, 56.15020856422052]
-    },
-    properties: {
-      title: 'Small item',
-      // description: 'Your moms underpants haha'
-    }
-  }]
-}
-
-async function please() {
+async function getData() {
   let response = await fetch('http://134.209.242.120');
-  if (response.ok) { // if HTTP-status is 200-299
-    // get the response body (the method explained below)
-    var json = await response.json();
+  if (response.ok) { 
+    let json = await response.json();
     json.forEach(function(marker) {
     
-      var el = document.createElement('div');
+      let el = document.createElement('div');
       
-      if (marker.type == 'Large' ) {
-        el.className = 'map-marker';
-      } else if (marker.type == 'Small' ) {
-        el.className = 'map-marker__red';
+      if (marker.type == 'large' ) {
+        el.className = 'map-marker__large';
+      } else if (marker.type == 'small' ) {
+        el.className = 'map-marker__small';
       } else {
-        el.className = 'map-marker__shrek';
+        el.className = 'map-marker__medium';
       }
   
-      // make a marker for each item and add to the map
       new mapboxgl.Marker(el)
       .setLngLat(marker.geo)
       .addTo(map);
@@ -75,4 +33,4 @@ async function please() {
   }
 }
 
-please();
+getData();
